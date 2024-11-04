2acda993f041cadb2eaeda442d972171b162f308
Mohit Kanojiya <mohitk21103@gmail.com>
2024-11-05 01:00:29 +0530

    Added QuestionScreen

--- app/src/main/java/mk/project/errorexposer/MainActivity.kt
+++ app/src/main/java/mk/project/errorexposer/MainActivity.kt
@@ -1,30 +1,30 @@
 package mk.project.errorexposer
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import mk.project.errorexposer.ui.theme.ErrorExposerTheme
+import androidx.lifecycle.viewmodel.compose.viewModel
 import mk.project.errorexposer.ui.theme.HomeView
+import mk.project.errorexposer.ui.theme.QuestionsScreen
 
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            ErrorExposerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    HomeView()
+            MaterialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    val qVM : QuestionsViewModel = viewModel()
+                    val qState by qVM.questionState
+                    QuestionsScreen(viewstate = qState)
                 }
             }
         }
