092b7bd0fd4ad2024e5cbeb85ec67a584591507c
Mohit Kanojiya <mohitk21103@gmail.com>
2024-11-05 00:58:30 +0530

    changed versions of some dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,12 +71,12 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     // ViewModel and Lifecycle
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
 
     // Core and Activity Compose
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-compose:1.9.3")
 
     // Testing Libraries
