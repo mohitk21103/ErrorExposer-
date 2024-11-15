14c1314415e6a42e795dc4a41b7f56df734d44cb
Mohit Kanojiya <mohitk21103@gmail.com>
2024-11-16 01:11:52 +0530

    Added Navigation function

--- app/src/main/java/mk/project/errorexposer/MainActivity.kt
+++ app/src/main/java/mk/project/errorexposer/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
 import mk.project.errorexposer.ui.theme.HomeView
 import mk.project.errorexposer.ui.theme.QuestionsScreen
 
@@ -18,13 +20,11 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
+            val navcontroller = rememberNavController()
             MaterialTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    val qVM : QuestionsViewModel = viewModel()
-                    val qState by qVM.questionState
-                    QuestionsScreen(viewstate = qState)
+                    Navigation(navController = navcontroller)
                 }
             }
         }
