5a0f8071bb39b1e403fa07594b12ff683da05669
Mohit Kanojiya <mohitk21103@gmail.com>
2024-11-16 01:19:34 +0530

    Optimized imports

--- app/src/main/java/mk/project/errorexposer/ui/theme/QuestionsScreen.kt
+++ app/src/main/java/mk/project/errorexposer/ui/theme/QuestionsScreen.kt
@@ -18,18 +18,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -39,7 +34,6 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
-import mk.project.errorexposer.Owner
 import mk.project.errorexposer.QuestionItem
 import mk.project.errorexposer.QuestionsViewModel
 
